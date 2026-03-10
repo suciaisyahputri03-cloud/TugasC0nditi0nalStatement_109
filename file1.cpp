bd7de0fbb52d887253a352560f17fad66f534736
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-10 22:35:32 +0800

    menambahkan fungsi hitungBMI untuk menghitung BMI

--- file1.cpp
+++ file1.cpp
@@ -2,3 +2,9 @@
 #include <string>
 
 using namespace std;
+
+//fungsi untuk menghitung nilai BMI
+//menggunakan rumus: BMI = berat / (tinggi * tinggi)
+float hitungBMI(float berat, float tinggi){
+    return berat / (tinggi * tinggi);
+}
