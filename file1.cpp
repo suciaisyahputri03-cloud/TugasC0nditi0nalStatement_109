3cad5fba680d3044959843524e06b9d4c060051c
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-10 22:41:43 +0800

    menambahkan fungsi cekstatus untuk menentukan kategori berat badan

--- file1.cpp
+++ file1.cpp
@@ -8,3 +8,16 @@ using namespace std;
 float hitungBMI(float berat, float tinggi){
     return berat / (tinggi * tinggi);
 }
+
+// fungsi untuk menentukan kategori satus berat badan 
+string cekstatus(float bmi){
+    if (bmi < 18.5){
+        return "berat badan kurang";
+    } else if (bmi < 25){
+        return "berat badan normal";
+    } else if (bmi < 30){
+        return "berat badan berlebihan";
+    } else {
+        return "obesitas";
+    }
+}
